a35db56f2c596a65cadc254a85ef1075e0983fad
imtaeyi <mintchodan@naver.com>
2022-11-04 17:20:25 +0900

    이니셜 프로젝트

---
+++ src/pages/Count.vue
@@ -0,0 +1,17 @@
+<template>
+  <button @click="count++">{{ count }}</button>
+</template>
+
+<script>
+export default {
+  // 컴포넌트
+  // data:{
+  //     counter:0
+  // }
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
